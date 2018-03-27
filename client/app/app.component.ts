991c7ff6c8c5bd4592220b0650f87b90973a1ad5
Everton Walker <everton.walker@hotmail.com>
2018-03-27 20:42:32 -0300

    Alteração Base

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -4,13 +4,9 @@ import { Component, Inject } from '@angular/core';
     moduleId: module.id,
     selector: 'app',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
 
-    public eae(){
-        console.log('clicou');
-    }
 
  }
 
